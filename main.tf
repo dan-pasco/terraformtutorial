331850f49d64dc18ad5085c22d3394d45fe7cfae
Dan Pasco <daniel_arc21@yahoo.com>
2022-02-02 17:13:42 +1300

    Security Group Added

--- main.tf
+++ main.tf
@@ -3,18 +3,33 @@ provider "aws" {
 }
 
 
+variable "ingressvar" {
+
+  type = list(number)
+  default = [ 80,443,22 ]
+  
+}
+
+
+variable "egressvar" {
+
+  type = list(number)
+  default = [ 80,443,22 ]
+  
+}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0c635ee4f691a2310"
   instance_type = "t2.micro"
-
+  security_groups = aws_security_group.EC2_SG.id
+  
+  
   tags = {
     Name = "MyExam"
-  
 }
 }
 
-output "Instance Private IP" {
+output "Instance_public_IP" {
     value = aws_instance.my_instance.public_ip
 }
 
@@ -22,6 +37,37 @@ resource "aws_eip" "lb" {
   instance = aws_instance.my_instance.id
 }
 
-resource "aws_security_groups" "EC2_SG" {
+resource "aws_security_group" "EC2_SG" {
+
+  name = "Allow Traffic"
+
+  dynamic "ingress"{
+    iterator = port
+    for_each = var.ingressvar
+    content{
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
+
+  dynamic "egress"{
+    iterator = port
+    for_each = var.egressvar
+    content{
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
   
+  tags = {
+
+    Name = "Allow Tls"
+
+  }
 }
